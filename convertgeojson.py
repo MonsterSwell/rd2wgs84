2630a2e9d30d795a4054337cb5f681ac38861c7e
Kilian <kilian@kilianvalkhof.com>
2011-01-18 13:26:16 +0100

    inverse translation for proper display

--- convertgeojson.py
+++ convertgeojson.py
@@ -6,10 +6,10 @@ import rd2wgs84
 
 
 def convertGeoJson(filename):
-"""
-  Translates GeoJson files in rijksdriehoeks format to WGS84
-  Assumes a valid GeoJson file
-"""
+    """
+    Translates GeoJson files in rijksdriehoeks format to WGS84
+    Assumes a valid GeoJson file
+    """
 
     geofile = open(filename)
     jsonFile = simplejson.loads(geofile.read())
@@ -19,13 +19,13 @@ def convertGeoJson(filename):
             for k in j:
                 if type(k[0]).__name__=='float':
                     newk = rd2wgs84.convert(k[0], k[1])
-                    k[0] = newk[0]
-                    k[1] = newk[1]
+                    k[0] = newk[1]
+                    k[1] = newk[0]
                 else:
                     for l in k:
                         newl = rd2wgs84.convert(l[0], l[1])
-                        l[0] = newl[0]
-                        l[1] = newl[1]
+                        l[0] = newl[1]
+                        l[1] = newl[0]
 
     newGeofile = open("wgs84." + geofile.name, "w")
     newGeofile.write(simplejson.dumps(jsonFile))
